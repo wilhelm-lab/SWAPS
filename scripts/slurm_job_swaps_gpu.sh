46157e4028ac67cf8c589fd83fb437af7af60970
zix-xiao <cory.zixuan.xiao@outlook.com>
2024-08-30 14:30:59 +0200

    ignore bash script

--- scripts/slurm_job_swaps_gpu.sh
+++ scripts/slurm_job_swaps_gpu.sh
@@ -6,14 +6,14 @@
 #SBATCH --time=24:00:00
 #SBATCH --output=/cmnfs/proj/ORIGINS/SWAPS_exp/slurm_out/%x_%j.out
 #SBATCH --job-name=swaps_PS
-#SBATCH --mem=200G
+#SBATCH --mem=80G
 #SBATCH --mail-user=zixuan.xiao@tum.de
 #SBATCH --mail-type=ALL
 #SBATCH --partition=shared-gpu
 #SBATCH --nodelist=gpu02.exbio.wzw.tum.de
 
 #config_path='/cmnfs/proj/ORIGINS/protMSD/maxquant/ScanByScan/utils/config_hela_120min_frac_100ms.yaml'
-config_path='/cmnfs/proj/ORIGINS/SWAPS_exp/tims_ramp_time/120min_libaray_100ms_20240826_175342_706954/config_seg_cls_1_1_minmaxscale_depth6.yaml'
+config_path='/cmnfs/proj/ORIGINS/SWAPS_exp/tims_ramp_time/120min_libaray_100ms_20240826_175342_706954/config_seg_cls_sep_depth6.yaml'
 source $HOME/condaInit.sh
 conda activate sbs
 python /cmnfs/proj/ORIGINS/protMSD/maxquant/ScanByScan/sbs_runner_ims.py --config_path=$config_path || exit 91
